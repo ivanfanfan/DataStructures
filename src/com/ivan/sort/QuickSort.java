5f77e513a1ea4a331c0ccee70df87754cbef7249
ivanfanfan <ivan_sir@163.com>
2020-09-21 17:36:36 +0800

    快速排序

--- src/com/ivan/sort/QuickSort.java
+++ src/com/ivan/sort/QuickSort.java
@@ -2,60 +2,49 @@ package com.ivan.sort;
 
 import java.util.Arrays;
 
+/**
+ * 画图，很简单
+ */
 public class QuickSort {
     public static void main(String[] args) {
-        int[] arr = {-1, 3, 2, 5, 10, 2};
+        int[] arr = {6, 8, 7, 4, 5, 3, 9};
+        int[] quick = quick(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(quick));
+    }
 
-        new QuickSort().quickSort(arr, 0, arr.length - 1);
+    public static int[] quick(int[] arr, int left, int right) {
+        if (left < right) {//递归结束条件
+            int partition = partition(arr, left, right);
+            quick(arr, left, partition - 1);
+            quick(arr, partition + 1, right);
+        }
+        return arr;
     }
 
     /**
-     * 定义指针指向最左边和最右边
-     * @param arr
-     * @param left
-     * @param right
+     * 将小的放在左边，大的放在右边
      */
-    public void quickSort(int[] arr, int left, int right) {
+    public static int partition(int[] arr, int left, int right) {
+        int pivot = left;
+        int index = pivot + 1;//指针 指向 第一个约定交换的位置
+        for (int i = index; i < right; i++) {//i 遍历指针 遍历后面的数
+            if (arr[i] < arr[pivot]) {
+                swap(arr, i, index);
+                index++; //交换后待交换数的位置得++  画图很明显
+            }
 
-        int l = left;
-        int r = right;
-        int temp;
-        int pivot = arr[(right + left) / 2];
-        while (l < r) {
-            //找值
-            while (arr[l] < pivot) {
-                l+=1;
-            }
-            while (arr[r] > pivot) {
-                r-=1;
-            }
-            if (l >= r) {
-                break;
-            }
-            //交换
-            temp = arr[r];
-            arr[r] = arr[l];
-            arr[l] = temp;
-            //交换完成后如果arr[l]=pivot l++
-            if (arr[l] == pivot) {
-                r-=1;
-            }
-            if (arr[r] == pivot) {
-                l+=1;
-            }
         }
-        //如果l == r 必须l++ r-- 否则栈溢出
-        if (l == r) {
-            l+=1;
-            r-=1;
-        }
-        //向左递归
-        if (left < r) {
-            quickSort(arr, left, r);
-        }
-        if (right > l) {
-            quickSort(arr, l, right);
-        }
-        System.out.println(Arrays.toString(arr));
+        swap(arr,pivot, index - 1);
+        return index - 1;
+
+    }
+
+    /**
+     * change
+     */
+    public static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
